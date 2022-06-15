4513676feaefea36b909c729622b30b82c47d30d
David <1192316306@qq.com>
2022-06-15 09:19:51 +0800

    spring

--- src/main/java/com/ch10/FirstController.java
+++ src/main/java/com/ch10/FirstController.java
@@ -2,6 +2,7 @@ package com.ch10;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FirstController {
@@ -9,5 +10,13 @@ public class FirstController {
     public String sayHello(){
         System.out.println("欢迎访问第一个springmvc页面");
         return "success";
+    }
+
+    @RequestMapping("/happy")
+    public ModelAndView sayHappy(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("happy","I'm happy!");
+        modelAndView.setViewName("happy");
+        return modelAndView;
     }
 }
